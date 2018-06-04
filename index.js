7b2bff4f0a30fb21292965c91b077bacf1c092c8
Stephen Crowell <crowelsl@rose-hulman.edu>
2018-06-04 17:39:07 -0400

    Finished Part 1

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const button = document.querySelector('button');
+button.addEventListener('click', function() {
+    const heading = document.querySelector('h1');
+    heading.textContent = 'Changed';
+});
