93568fbe82778c8586f3dce961218d35b1167653
Stephen Crowell <crowelsl@rose-hulman.edu>
2018-06-05 09:15:51 -0400

    Added a reset button

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const button = document.querySelector('button');
+const reset = document.getElementById('reset');
+
 button.addEventListener('click', function() {
     const heading = document.getElementById('1');
     heading.textContent = 'Changed';
 });
+
+reset.addEventListener('click', function() {
+    let heading = document.getElementById('0');
+    for(let i = 0; i < 7; i++) {
+        heading.textContent = 'Spellbook ' + i;
+        heading = document.getElementById(i + 1);
+    }
+})
 
 const form = document.querySelector('form');
 const submitButton = form[1];
 
 changeText = function() {
     const heading = document.getElementById(document.querySelector('input[name = "spellbook"]:checked').value);
+    if(form[0].value === '') {
+        return;
+    }
     heading.textContent = form[0].value;
 }
 
