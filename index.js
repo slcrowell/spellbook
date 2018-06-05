09f9386d33dd6a3091738b68a76fe207d783a0f7
Stephen Crowell <crowelsl@rose-hulman.edu>
2018-06-05 17:36:53 -0400

    add a spell by using createElement and appendChild

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const form = document.querySelector('form');
+const schools = ["evocation", "enchantment", "conjuration", "transmutation", "divination", "illusion", "abjuration", "necromancy"]
+const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffa500', '#aa00ff', '#ffff00', '#ffffff', '#000000']
 
-changeText = function() {
-    const spell = form.spellName.value;
+addSpell = function() {
     const spellsDiv = document.querySelector('#spells');
-    spellsDiv.innerHTML += `<li>${spell} requires level ${form.spellLevel.value}</li>`;
-    form.spellName.reset();
+    const label = document.createElement('l1');
+    label.appendChild(document.createTextNode(`${form.spellName.value} requires level ${form.spellLevel.value}`));
+    label.appendChild(document.createElement('br')); 
+    console.log(schools.indexOf(form.school.value.toLowerCase()));   
+    if(schools.indexOf(form.school.value) != -1) {
+        label.style.color = colors[schools.indexOf(form.school.value)];
+    }
+    spellsDiv.appendChild(label);
+    form.spellName.value = '';
+    form.spellLevel.value = '';
+    form.school.value = '';
 };
 
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    changeText();
+    addSpell();
 }, false);
