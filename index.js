92895b789311db063d7f08158f7e8e1f0a426ce0
Stephen Crowell <crowelsl@rose-hulman.edu>
2018-06-04 23:41:36 -0400

    Finished the last two parts

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ button.addEventListener('click', function() {
     const heading = document.getElementById('head1');
     heading.textContent = 'Changed';
 });
+
+const form = document.querySelector('form');
+const submitButton = form[1];
+
+changeText = function() {
+    const heading = document.getElementById('head1');
+    heading.textContent = form[0].value;
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    changeText();
+}, false);
+
+//submitButton.addEventListener('click', changeText);
