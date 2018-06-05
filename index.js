318215902099fbd480c9d09a9f3f0c87f6290bcc
Stephen Crowell <crowelsl@rose-hulman.edu>
2018-06-05 17:10:27 -0400

    have a second form for the level required for the spell. It will append.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const form = document.querySelector('form');
 
 changeText = function() {
     const spell = form.spellName.value;
-    const spellsDiv = document.querySelector('#spells')
-    spellsDiv.innerHTML += `<li>${spell}</li>`
+    const spellsDiv = document.querySelector('#spells');
+    spellsDiv.innerHTML += `<li>${spell} requires level ${form.spellLevel.value}</li>`;
     form.spellName.reset();
-}
+};
 
 
 form.addEventListener('submit', function(e) {
