3de20ea92937747d349b5332e93db0fe1545352a
Stephen Crowell <crowelsl@rose-hulman.edu>
2018-06-06 16:58:46 -0400

    contain code from class

--- index.js
+++ index.js
@@ -1,62 +1,55 @@
-const form = document.querySelector('form');
-
-addSpell = function() {
-    const spellsDiv = document.querySelector('#spells');
-    if(form.spellName.value == '' || form.spellDescription.value =='') {
-        alert('EMPTY BOX. FAILED TO ADD SPELL');
-        return;
-    }
-
-    spellsDiv.appendChild(addLabel());
-    form.spellName.value = '';
-    form.spellDescription.value = '';
-};
-
-addLabel = function() {
-    const label = document.createElement('l1');
-
-    label.appendChild(addNameSpan());
-    label.appendChild(addSchool());
-    label.appendChild(addDescipSpan());   
-    label.appendChild(addLevelSpan());
-    label.appendChild(document.createElement('br'));
-    return label;
-}
-
-addNameSpan = function() {
-    const spellSpan = document.createElement('span');
-    spellSpan.appendChild(document.createTextNode(`${form.spellName.value} `));
-    spellSpan.style.fontFamily = 'UnifrakturCook, sans-serif';
-    spellSpan.setAttribute('class', 'spellName');
-    return spellSpan;
-}
-
-addDescipSpan = function() {
-    const descripSpan = document.createElement('span');
-    descripSpan.appendChild(document.createTextNode(` ${form.spellDescription.value}`))
-    descripSpan.style.fontFamily = 'Nanum Pen Script, sans-serif';
-    descripSpan.setAttribute('class', 'spellDescrip');
-    return descripSpan;
-}
-
-addLevelSpan = function() {
-    const levelSpan = document.createElement('span');
-    levelSpan.appendChild(document.createTextNode(` and requires level ${form.spellLevel.options[form.spellLevel.selectedIndex].value}.`))
-    levelSpan.style.fontFamily = 'Indie Flower, sans-serif';
-    levelSpan.setAttribute('class', 'spellLevel');
-    return levelSpan;
-}
-
-addSchool = function() {
-    const schoolSpan = document.createElement('span');
-    document.getElementById('school').src = form.school.options[form.school.selectedIndex].value + ' symbol.jpg';   
-    schoolSpan.appendChild(document.createTextNode(` (${form.school.options[form.school.selectedIndex].value})`));
-    schoolSpan.style.fontFamily = 'Kaushan Script, sans-serif';
-    schoolSpan.setAttribute('class', 'school');
-    return schoolSpan;
-}
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    addSpell();
-}, false);
+const app = {
+    init: function() {
+      const form = document.querySelector('form')
+      form.addEventListener('submit', (ev) => {
+        ev.preventDefault()
+        this.handleSubmit(ev)
+      })
+    },
+  
+    renderProperty: function(name, value) {
+      const el = document.createElement('span')
+      el.classList.add(name)
+      el.textContent = value
+      el.setAttribute('title', value)
+      return el
+    },
+  
+    renderItem: function(spell) {
+      // ['name', 'level']
+      const properties = Object.keys(spell)
+  
+      // collect an array of <span> elements
+      const childElements = properties.map((prop) => {
+        return this.renderProperty(prop, spell[prop])
+      })
+  
+      const item = document.createElement('li')
+      item.classList.add('spell')
+  
+      // append each <span> to the <li>
+      childElements.forEach(function(el) {
+        item.appendChild(el)
+      })
+  
+      return item
+    },
+  
+    handleSubmit: function(ev) {
+      const f = ev.target
+  
+      const spell = {
+        name: f.spellName.value,
+        level: f.level.value,
+      }
+  
+      const item = this.renderItem(spell)
+  
+      const list = document.querySelector('#spells')
+      list.appendChild(item)
+  
+      f.reset()
+    },
+  }
+  
+  app.init()
