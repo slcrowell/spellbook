e52d8e2a7e22369e02f9998e8f5e94f65cfbf6a8
Stephen Crowell <crowelsl@rose-hulman.edu>
2018-06-06 17:13:22 -0400

    add spells to an array.

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
+const spellList = [];
 const app = {
     init: function() {
-      const form = document.querySelector('form')
+      const form = document.querySelector('form');
       form.addEventListener('submit', (ev) => {
-        ev.preventDefault()
-        this.handleSubmit(ev)
-      })
+        ev.preventDefault();
+        this.handleSubmit(ev);
+      });
     },
   
     renderProperty: function(name, value) {
-      const el = document.createElement('span')
-      el.classList.add(name)
-      el.textContent = value
-      el.setAttribute('title', value)
-      return el
+      const el = document.createElement('span');
+      el.classList.add(name);
+      el.textContent = value;
+      el.setAttribute('title', value);
+      return el;
     },
   
     renderItem: function(spell) {
       // ['name', 'level']
-      const properties = Object.keys(spell)
+      const properties = Object.keys(spell);
   
       // collect an array of <span> elements
       const childElements = properties.map((prop) => {
-        return this.renderProperty(prop, spell[prop])
-      })
+        return this.renderProperty(prop, spell[prop]);
+      });
   
-      const item = document.createElement('li')
-      item.classList.add('spell')
+      const item = document.createElement('li');
+      item.classList.add('spell');
   
       // append each <span> to the <li>
       childElements.forEach(function(el) {
-        item.appendChild(el)
-      })
+        item.appendChild(el);
+      });
   
-      return item
+      return item;
     },
   
     handleSubmit: function(ev) {
-      const f = ev.target
+      const f = ev.target;
   
       const spell = {
         name: f.spellName.value,
         level: f.level.value,
-      }
+      };
+      
+      spellList.push(spell);
   
-      const item = this.renderItem(spell)
-  
-      const list = document.querySelector('#spells')
-      list.appendChild(item)
-  
-      f.reset()
+      this.renderList();
+      f.reset();
     },
+
+    renderList: function() {
+        const list = document.querySelector('#spells');
+        spellList.forEach((spell) => {
+            list.appendChild(this.renderItem(spell));
+        });
+    }
   }
   
-  app.init()
+  app.init();
